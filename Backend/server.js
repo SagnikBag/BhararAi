457a23656199bd9d961a57ee31134fb27b874166
sagnik bag <sagnikbag2001@gmail.com>
2026-05-27 17:25:15 +0530

    feat: add socket.io integration for real-time communication and update server setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,16 @@
 import  'dotenv/config';
 import app from './src/app.js';
+import http from 'http';
 import connectDB from './src/config/database.js';
 import { testAi } from './src/services/ai.service.js';
+import { initSocket } from './src/sockets/server.socket.js';
 const PORT = process.env.PORT ||3000;
 
 testAi();
+
+const httpServer = http.createServer(app);
+
+initSocket(httpServer);
 
 connectDB()
 .catch((err)=>{
@@ -12,7 +18,7 @@ connectDB()
     
 })
 
-app.listen(PORT,()=>{
+httpServer.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
     
 })
