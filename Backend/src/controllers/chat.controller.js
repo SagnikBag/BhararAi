3b1758d4cc5dc1a11049bda08ac33818a6f8b844
sagnik bag <sagnikbag2001@gmail.com>
2026-05-31 02:33:07 +0530

    create getchat,getmessage,deletechat

--- Backend/src/controllers/chat.controller.js
+++ Backend/src/controllers/chat.controller.js
@@ -1,6 +1,7 @@
 import {generateResponse,generateChatTitle} from '../services/ai.service.js';
 import ChatModel from '../models/chat.model.js';
 import messageModel from '../models/message.model.js';
+import chatModel from '../models/chat.model.js';
 
 
 
@@ -25,11 +26,9 @@ if(!chatId){
         role: "user"
     })
 
-    const messages = await messageModel.find({chat: chatId})
+    const messages = await messageModel.find({chat: chatId || chat._id})
 
     const result =  await generateResponse(messages);
-
-    
 
     const aiMessage = await messageModel.create({
      chat: chatId ||chat._id,
@@ -37,15 +36,62 @@ if(!chatId){
      role: "ai"
    })
 
-    
-       
-
-  
-console.log(messages);
+    console.log(messages);
     res.status(201).json({
         title,
         chat,
-        aiMessage
+        aiMessage:result
     })
 }
+export async function getChats(req,res){
+    const user = req.user;
 
+    const chats = await ChatModel.find({user: user.id})
+
+    res.status(200).json({
+        message: "Chats retrieved successfully",
+        chats
+    })
+}
+export async function getMessages(req,res){
+    const {chatId} = req.params;
+
+    const chat = await chatModel.findOne({
+        _id: chatId,
+        user: req.user.id
+    })
+
+    if(!chat){
+        return res.status(404).json({
+            message: "chat not found"
+        })
+    }
+    const messages = await messageModel.find({
+        chat:chatId
+    })
+
+    res.status(200).json({
+        message: "Messages retrieved successfullt",
+        messages
+    })
+}
+export async function deletChat(req,res){
+    const {chatId} = req.params;
+
+    const chat = await chatModel.findOneAndDelete({
+        _id: chatId,
+        user: req.user.id
+    })
+
+    if(!chat){
+        return res.status(404).json({
+            message: "chat not found"
+        })
+    }
+    await messageModel.deleteMany({
+        chat:chatId
+    })
+    res.status(200).json({
+        message: "Chat deleted successfully"
+    })
+}
--- Backend/src/routes/chat.route.js
+++ Backend/src/routes/chat.route.js
@@ -1,11 +1,14 @@
 import {Router} from 'express';
-import {sendMessage} from '../controllers/chat.controller.js';
+import {sendMessage,getChats,getMessages,deletChat} from '../controllers/chat.controller.js';
 import {authUser} from '../middlewares/auth.middleware.js';
 
 
 const chatRouter = Router();
 
 chatRouter.post("/message",authUser,sendMessage);
+chatRouter.get("/",authUser,getChats);
+chatRouter.get("/:chatId/messages",authUser,getMessages);
+chatRouter.delete("/:chatId",authUser,deletChat);
 
  
 
